0677e4b047e62c2d4ecf0157f3cc57bcc15b382d
Ivan-F3nDz3 <ivanfenyom@gmail.com>
2024-05-07 12:11:18 +0900

    Update DRL3.m

--- DRL/DRL3.m
+++ DRL/DRL3.m
@@ -21,7 +21,7 @@ classdef DRL3 < PROBLEM
         function Setting(obj)
             obj.M = 6;
             if isempty(obj.D); obj.D = 4482; end
-            obj.lower     = zeros(1,obj.D);
+            obj.lower     = ones(1,obj.D)*-1;
             obj.upper     = ones(1,obj.D);
             obj.encodding = ones(1,obj.D);
 
