dd32b15ed1247833c99e0d2905187ce3f474e0ac
Ivan-F3nDz3 <ivanfenyom@gmail.com>
2024-05-07 11:48:07 +0900

    Update DRL3.m

--- DRL/DRL3.m
+++ DRL/DRL3.m
@@ -21,8 +21,9 @@ classdef DRL3 < PROBLEM
         function Setting(obj)
             obj.M = 6;
             if isempty(obj.D); obj.D = 4482; end
-            obj.lower    = zeros(1,obj.D) - 1;
-            obj.upper    = zeros(1,obj.D) + 1;
+            obj.lower     = zeros(1,obj.D);
+            obj.upper     = ones(1,obj.D);
+            obj.encodding = ones(1,obj.D);
 
         end
         %% Calculate objective values
