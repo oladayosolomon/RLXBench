3faac8f4a98ca8eeba805ade2f67e819ffb1e76e
Ivan-F3nDz3 <ivanfenyom@gmail.com>
2024-05-07 11:47:40 +0900

    Update DRL2.m

--- DRL/DRL2.m
+++ DRL/DRL2.m
@@ -21,8 +21,9 @@ classdef DRL2 < PROBLEM
         function Setting(obj)
             obj.M = 2;
             if isempty(obj.D); obj.D = 4612; end
-            obj.lower    = zeros(1,obj.D) - 1;
-            obj.upper    = zeros(1,obj.D) + 1;
+            obj.lower     = zeros(1,obj.D);
+            obj.upper     = ones(1,obj.D);
+            obj.encodding = ones(1,obj.D);
         end
         %% Calculate objective values
         function PopObj = CalObj(obj,X)
